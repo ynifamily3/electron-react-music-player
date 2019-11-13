bab02508a60a7600c46b979737b78547506cbfb2
Jongkeun Choi <ynifamily3@gmail.com>
2019-11-14 00:38:03 +0900

    초기설정
    Signed-off-by: Jongkeun Choi <ynifamily3@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import Cp from './components/comp'; // 컴포넌트명은 꼭 대문자로.
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      comp load test2 : <br/> <Cp text='test테스트'/>
     </div>
   );
 }
---
+++ src/components/comp.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const comp = props => {
+  return <div>컴포넌트입니당! - {props.text}</div>;
+};
+
+export default comp;
