ae7c9c9272c48280cfcd2e205a314f4e9a20b37b
jongkeun choi <ynifamily3@gmail.com>
2019-11-19 15:58:17 +0900

    리스트 일부 구현

--- src/components/LyPlaylist/PlaylistSection/ListItems/ListItems.jsx
+++ src/components/LyPlaylist/PlaylistSection/ListItems/ListItems.jsx
@@ -3,7 +3,10 @@ import { createUseStyles } from "react-jss";
 import clsx from "clsx";
 
 const useStyles = createUseStyles({
-  listItems: {
+  listItem: {
+
+  },
+  thumb: {
 
   }
 });
@@ -11,9 +14,25 @@ const useStyles = createUseStyles({
 const ListItems = props => {
   const classes = useStyles();
   return (
-    <div className={classes.listItems}>
-      리스트 아이템들.
-    </div>
+    <ul>
+      <li className={classes.listItem}>
+        <div className={classes.thumb}>
+          <img src={`https://musicmeta-phinf.pstatic.net/album/000/004/4379.jpg?type=r100Fll&v=20190823160604`} alt={`Savage Garden 앨범 커버`} />
+        </div>
+        <div className={classes.infoArea}>
+          <em className={classes.songArea}>
+            <span className={classes.inner}>
+              <span className={classes.song}>I want You</span>
+            </span>
+          </em>
+          <span className={classes.artist}>Savage Garden</span>
+        </div>
+        <a href="#" className={classes.iconPlay}></a>
+        <a href="#" className={classes.btnDelete}></a>
+        <a href="#" className={classes.btnMove}></a>
+        <a href="#" className={classes.btnMove}></a>
+      </li>
+    </ul>
   );
 };
 
--- src/components/LyPlaylist/PlaylistSection/PlaylistSection.jsx
+++ src/components/LyPlaylist/PlaylistSection/PlaylistSection.jsx
@@ -11,7 +11,38 @@ const useStyles = createUseStyles({
     bottom: "0",
     width: "350px",
     backgroundColor: "#141414",
-    color: "white" // debug
+  },
+  titleArea: {
+    position: "relative",
+    margin: "26px 0 0 20px"
+  },
+  title: {
+    fontSize: "15px",
+    lineHeight: "19px",
+    fontWeight: "700",
+    color: "#fff"
+  },
+  controlArea: {
+    position: "absolute",
+    top: "-7px",
+    right: "13px"
+  },
+  listWrap: {
+    position: "absolute",
+    top: "62px",
+    right: "0",
+    bottom: "0",
+    left: "0",
+    overflowX: "hidden"
+  },
+  normalArea: {
+    overflowX: "hidden",
+    overflowY: "auto",
+    position: "absolute",
+    top: "0",
+    right: "0",
+    bottom: "0",
+    left: "0"
   }
 });
 
@@ -22,8 +53,8 @@ const PlaylistSection = props => {
       <div className={classes.titleArea}>
         <h2 className={classes.title}>이어지는 노래</h2>
         <div className={classes.controlArea}>
-          <a title="재생 방법 변경" classname={classes.btnShuffle}></a>
-          <a title="반복 재생 설정" classname={classes.btnRepeat}></a>
+          <a title="재생 방법 변경" className={classes.btnShuffle}></a>
+          <a title="반복 재생 설정" className={classes.btnRepeat}></a>
         </div>
       </div>
       <div className={classes.listWrap}>
